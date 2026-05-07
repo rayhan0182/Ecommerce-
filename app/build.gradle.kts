de63cb4a0c744bd70b8943e4df6e077ad4f75dda
Rayhan <mdrayhan8045@gmail.com>
2026-05-07 21:19:31 +0600

    hilt implement

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,39 +1,47 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt.android)
+    kotlin("kapt")
+
 }
 
 android {
     namespace = "com.example.ecommerce"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
+    // Standard way to write this
 
     defaultConfig {
         applicationId = "com.example.ecommerce"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 34 // 34 is the current stable target
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-}
+
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(
+                org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+            )
+        }
+    }
+
+
+} // <--- Make sure this brace closes the 'android' block
 
 dependencies {
     implementation(libs.androidx.activity.ktx)
@@ -44,4 +52,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+
+    // Hilt
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.51.1")
 }
