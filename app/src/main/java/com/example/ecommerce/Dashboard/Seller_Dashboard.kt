76ba75d419c77d9f27d64297403a98da0d06aa12
Rayhan <mdrayhan8045@gmail.com>
2026-05-16 15:00:32 +0600

    top menu bar

--- app/src/main/java/com/example/ecommerce/Dashboard/Seller_Dashboard.kt
+++ app/src/main/java/com/example/ecommerce/Dashboard/Seller_Dashboard.kt
@@ -1,7 +1,11 @@
 package com.example.ecommerce.Dashboard
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,12 +17,19 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.ecommerce.R
 import com.example.ecommerce.databinding.ActivitySellerDashboardBinding
+import com.example.ecommerce.view.LandingFragment
+import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class Seller_Dashboard : AppCompatActivity() {
     lateinit var binding: ActivitySellerDashboardBinding
     lateinit var navController: NavController
+
+    @Inject
+    lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySellerDashboardBinding.inflate(layoutInflater)
@@ -26,7 +37,7 @@ class Seller_Dashboard : AppCompatActivity() {
 
         navController = findNavController(R.id.fragmentContainerView2)
 
-        val appbarConfigure = AppBarConfiguration(setOf(
+        val appconfiguration = AppBarConfiguration(setOf(
 
             R.id.Product,
 
@@ -36,8 +47,49 @@ class Seller_Dashboard : AppCompatActivity() {
 
         ))
 
-        binding.bottomNavi.setupWithNavController(navController)
+       binding.bottomNavi.setupWithNavController(navController)
 
-        setupActionBarWithNavController(navController,appbarConfigure)
+       setupActionBarWithNavController(navController,appconfiguration)
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_bar,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+
+        R.id.setting->{
+
+            Toast.makeText(this@Seller_Dashboard,"setting successfull created", Toast.LENGTH_LONG).show()
+
+        }
+
+            R.id.logout->{
+
+                auth.signOut()
+
+                startActivity(Intent(this@Seller_Dashboard, LandingFragment::class.java))
+
+            }
+
+            R.id.report->{
+
+                Toast.makeText(this@Seller_Dashboard,"Report successfull created", Toast.LENGTH_LONG).show()
+
+            }
+
+        }
+
+
+
+
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
